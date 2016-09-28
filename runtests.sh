4c6bc1b18664a83dfcc51f5f4c092bb2500554e2
Angel Delgado <angeldelgado7@gmail.com>
2016-09-27 23:07:30 -0400

    updated test script

--- runtests.sh
+++ runtests.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #runtests.sh
-./A1 -f hw1_1.ps > out1.xpm
-./A1 -f hw1_2.ps > out2.xpm
-./A1 -f hw1_3.ps > out3.xpm
-./A1 -f hw1_4.ps > out4.xpm
-./A1 -f hw1_5.ps > out5.xpm
-./A1 -f hw1_6.ps > out6.xpm
-./A1 -f hw1_7.ps > out7.xpm
-./A1 -f hw1_8.ps > out8.xpm
+ant build
+cd bin
+./java edu.drexel.cs430.assignments.A1 -f hw1_1.ps > ../out1.xpm
+./java edu.drexel.cs430.assignments.A1 -f hw1_2.ps > ../out2.xpm
+./java edu.drexel.cs430.assignments.A1 -f hw1_3.ps > ../out3.xpm
+./java edu.drexel.cs430.assignments.A1 -f hw1_4.ps > ../out4.xpm
+./java edu.drexel.cs430.assignments.A1 -f hw1_5.ps > ../out5.xpm
+./java edu.drexel.cs430.assignments.A1 -f hw1_6.ps > ../out6.xpm
+./java edu.drexel.cs430.assignments.A1 -f hw1_7.ps > ../out7.xpm
+./java edu.drexel.cs430.assignments.A1 -f hw1_8.ps > ../out8.xpm
 
 
 
