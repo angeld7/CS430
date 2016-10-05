e73d987f7369c3bd178db16e594752da521f9016
Angel Delgado <angeldelgado7@gmail.com>
2016-10-04 21:17:36 -0400

    Working on A2 some more

--- src/edu/drexel/cs430/renderengine/render/LineRenderer.java
+++ src/edu/drexel/cs430/renderengine/render/LineRenderer.java
@@ -23,64 +23,64 @@ public class LineRenderer {
         Point end = line.getEndPoint();
         if (start.getX() == end.getX()) {
             //Horizontal line
-            int x = start.getX();
-            int startY = Math.min(start.getY(), end.getY());
-            int endY = Math.max(start.getY(), end.getY());
-            for (int y = startY; y <= endY; y++) {
-                pixelMatrix[y][x] = true;
+            float x = start.getX();
+            float startY = Math.min(start.getY(), end.getY());
+            float endY = Math.max(start.getY(), end.getY());
+            for (float y = startY; y <= endY; y++) {
+                pixelMatrix[Math.round(y)][Math.round(x)] = true;
             }
         } else if (start.getY() == end.getY()) {
             //Vertical line
-            int y = start.getY();
-            int startX = Math.min(start.getX(), end.getX());
-            int endX = Math.max(start.getX(), end.getX());
-            for (int x = startX; x <= endX; x++) {
-                pixelMatrix[y][x] = true;
+            float y = start.getY();
+            float startX = Math.min(start.getX(), end.getX());
+            float endX = Math.max(start.getX(), end.getX());
+            for (float x = startX; x <= endX; x++) {
+                pixelMatrix[Math.round(y)][Math.round(x)] = true;
             }
         } else {
             float slope = (end.getY() - start.getY()) / (float) (end.getX() - start.getX());
-            int dx = Math.abs(end.getX() - start.getX());
-            int dy = Math.abs(end.getY() - start.getY());
-            int D = 2 * dy - dx;
+            float dx = Math.abs(end.getX() - start.getX());
+            float dy = Math.abs(end.getY() - start.getY());
+            float D = 2 * dy - dx;
             if (slope > 0 && slope <= 1) {
                 if (start.getX() > end.getX()) swap(start, end);
-                int y = start.getY();
-                for (int x = start.getX(); x <= end.getX(); x++) {
-                    pixelMatrix[y][x] = true;
+                float y = start.getY();
+                for (float x = start.getX(); x <= end.getX(); x++) {
+                    pixelMatrix[Math.round(y)][Math.round(x)] = true;
                     if ((D = nextD(D, dx, dy)) > 0) y++;
                 }
             } else if (slope > 1) {
                 if (start.getY() > end.getY()) swap(start, end);
-                int x = start.getX();
-                for (int y = start.getY(); y <= end.getY(); y++) {
-                    pixelMatrix[y][x] = true;
+                float x = start.getX();
+                for (float y = start.getY(); y <= end.getY(); y++) {
+                    pixelMatrix[Math.round(y)][Math.round(x)] = true;
                     if ((D = nextD(D, dy, dx)) > 0) x++;
                 }
             } else if (slope < 0 && slope >= -1) {
                 if (start.getX() > end.getX()) swap(start, end);
-                int y = start.getY();
-                for (int x = start.getX(); x <= end.getX(); x++) {
-                    pixelMatrix[y][x] = true;
+                float y = start.getY();
+                for (float x = start.getX(); x <= end.getX(); x++) {
+                    pixelMatrix[Math.round(y)][Math.round(x)] = true;
                     if ((D = nextD(D, dx, dy)) > 0) y--;
                 }
             } else {
                 if (start.getY() > end.getY()) swap(start, end);
-                int x = start.getX();
-                for (int y = start.getY(); y <= end.getY(); y++) {
-                    pixelMatrix[y][x] = true;
+                float x = start.getX();
+                for (float y = start.getY(); y <= end.getY(); y++) {
+                    pixelMatrix[Math.round(y)][Math.round(x)] = true;
                     if ((D = nextD(D, dy, dx)) > 0) x--;
                 }
             }
         }
     }
 
-    private int nextD(int D, int n1, int n2) {
+    private float nextD(float D, float n1, float n2) {
         D += D <= 0 ? 2 * n2 : 2 * (n2 - n1);
         return D;
     }
 
     private void swap(Point s, Point e) {
-        int tX = s.getX(), tY = s.getY();
+        float tX = s.getX(), tY = s.getY();
         s.setX(e.getX());
         s.setY(e.getY());
         e.setX(tX);
@@ -91,16 +91,37 @@ public class LineRenderer {
         Point s = line.getStartPoint(), e = line.getEndPoint();
         int b1 = getBitCode(s), b2 = getBitCode(e);
         if((b1 & b2) != 0) return false;
-        while((b1 | b2) != 0) {
-            if(b1 != 0) b1 = getBitCode(clipPoint(s));
-            if(b2 != 0) b2 = getBitCode(clipPoint(e));
+        if((b1 | b2) != 0) {
+            if(b1 != 0) clipPoint(s, e, b1);
+            if(b2 != 0) clipPoint(e, s, b2);
         }
         return true;
     }
 
-    private Point clipPoint(Point point) {
-        //TODO IMPLEMENT!!
+    private Point clipPoint(Point point, Point ref, int bitCode) {
+        for(int b = 1; b <= 8; b *= 2) {
+            if((b | bitCode) == b) movePoint(point, ref, b);
+        }
         return point;
+    }
+
+    private void movePoint(Point point, Point ref, int bit) {
+        switch(bit) {
+            case 1:
+                point.setY(proportion(point.getX(),point.getY(), ref.getX(), ref.getY()));
+                point.setX(0);
+                break;
+            case 2:
+                break;
+            case 4:
+                break;
+            case 8:
+                break;
+        }
+    }
+
+    private float proportion(float a, float b, float c, float d) {
+        return (-a / (a - c)) * (d - b) + b;
     }
 
     private int getBitCode(Point point) {
--- src/edu/drexel/cs430/renderengine/shapes/Point.java
+++ src/edu/drexel/cs430/renderengine/shapes/Point.java
@@ -4,27 +4,27 @@ package edu.drexel.cs430.renderengine.shapes;
  * Created by Angel on 9/25/2016.
  */
 public class Point {
-    private int x;
-    private int y;
+    private float x;
+    private float y;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public int getX() {
+    public float getX() {
         return x;
     }
 
-    public int getY() {
+    public float getY() {
         return y;
     }
 
-    public void setX(int x) {
+    public void setX(float x) {
         this.x = x;
     }
 
-    public void setY(int y) {
+    public void setY(float y) {
         this.y = y;
     }
 }
